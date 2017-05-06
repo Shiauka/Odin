a5c7acb9e2bdc546cd870f530d4f781abb2aa846
shiauka <eric741216@gmail.com>
2017-05-06 18:21:45 +0800

    update parserTWSE

--- Gungnir/script/parsingTWSE.sh
+++ Gungnir/script/parsingTWSE.sh
@@ -1,21 +1,22 @@
-code=$1
-month=$2
-binpath=$3
-path=$4
-out=$5
+day=$1
+binpath=$2
+path=$3
+out=$4
+para=$5
 
-output_sk="$out"/"$code".price
-output_list=$path/"$code".list
+output_sk=$out/"$para".price
+output_list=$path/"$para".list
 
-mkdir -p $path/$code
-python $binpath/month.py $month | while read monthline; do
-	echo "$code : $monthline"
-	wget -q -O $path/"$code"/"$monthline"_"$code".csv "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report"$monthline"/"$monthline"_F3_1_8_"$code".php&type=csv"
-    iconv -f big5 -t utf-8 "$path/"$code"/"$monthline"_"$code".csv" -o "$path/"$code"/"$monthline"_"$code".csv"
+mkdir -p $path/$para
+python $binpath/twse_date.py $day | while read dayline; do
+	echo "$dayline"
+	wget -q -O $path/$para/"$dayline".csv.big5 "http://www.tse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=csv&qdate="$dayline"&selectType="$para
+    iconv -f big5 -t utf-8 "$path/$para/"$dayline".csv.big5" -o "$path/$para/"$dayline".csv"
+	rm "$path/$para/"$dayline".csv.big5"
 done
-ls -RD $path/$code > "$output_list"
+ls -RD $path/$para > "$output_list"
 
-./bin/cvs2sk 0 $code $output_list $output_sk
+#./bin/cvs2sk 0 $code $output_list $output_sk
 
-rm -rf $output_list
+#rm -rf $output_list
 
