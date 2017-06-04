2799668d6ca222d601a1d88aaf6b4d40f38165a4
shiauka <shiauka@github.com>
2017-06-04 22:17:14 +0800

    refine TWSE parser for new TWSE website

--- Gungnir/script/parsingTWSE.sh
+++ Gungnir/script/parsingTWSE.sh
@@ -7,11 +7,14 @@ para=$5
 output_sk=$out/"$para".price
 output_list=$path/"$para".list
 
+
 mkdir -p $path/$para
 python $binpath/twse_date.py $day | while read dayline; do
 	echo "$dayline"
-	wget -q -O $path/$para/"$dayline".csv.big5 "http://www.tse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=csv&qdate="$dayline"&selectType="$para
-    iconv -f big5 -t utf-8 "$path/$para/"$dayline".csv.big5" -o "$path/$para/"$dayline".csv"
+	sample="http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date="$dayline"&type="$para
+	echo $sample
+	wget -q -O $path/$para/"$dayline".csv.big5 $sample
+    	iconv -f big5 -t utf-8 "$path/$para/"$dayline".csv.big5" -o "$path/$para/"$dayline".csv"
 	rm "$path/$para/"$dayline".csv.big5"
 done
 ls -RD $path/$para > "$output_list"
--- Gungnir/script/twse_date.py
+++ Gungnir/script/twse_date.py
@@ -4,14 +4,15 @@ import datetime
 from datetime import timedelta
 
 def main():
-	pattern = "%2F"
+	#pattern = "%2F"
 	Today = datetime.date.today()
 	a_day = timedelta(days=1)
 	for i in range(string.atoi(sys.argv[1])):
 		year = Today.year
 		month = Today.month
 		day = Today.day
-		print "%d%s%02d%s%02d" %(year-1911,pattern, month, pattern, day)
+		#print "%d%s%02d%s%02d" %(year-1911,pattern, month, pattern, day)
+		print "%d%02d%02d" %(year, month, day) 
 		Today -= a_day
 		
 		
